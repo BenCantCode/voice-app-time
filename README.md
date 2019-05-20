6d03b1dd6f2909894b8fa65c6142ac116de47253
Ben <benthegarvin@gmail.com>
2019-05-20 10:28:10 -0700

    Added git

---
+++ README.md
@@ -0,0 +1,5 @@
+## Greetings Snips
+
+This is based on snips-app-template by Snips.
+[Link to original project.](https://github.com/snipsco/snips-app-template-py)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/snipsco/snips-app-template-py/blob/master/LICENSE)
---
+++ action-app_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from snipsTools import SnipsConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+from datetime import datetime
+
+
+CONFIG_INI = "config.ini"
+
+# If this skill is supposed to run on the satellite,
+# please get this mqtt connection info from <config.ini>
+# Hint: MQTT server is always running on the master device
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+
+class Time(object):
+    """Class used to wrap action code with mqtt connection
+
+        Please change the name refering to your application
+    """
+
+    def __init__(self):
+        self.start_blocking()
+
+    # --> Master callback function, triggered everytime an intent is recognized
+    def master_intent_callback(self, hermes, intent_message):
+        coming_intent = intent_message.intent.intent_name
+        if(coming_intent == 'time'):
+            hermes.publish_end_session(intent_message.session_id, "")
+            hermes.publish_start_session_notification(intent_message.site_id, "It is" + datetime.now().strftime("%-I:%m %p"), "")
+
+    # --> Register callback function and start MQTT
+    def start_blocking(self):
+        with Hermes(MQTT_ADDR) as h:
+            h.subscribe_intents(self.master_intent_callback).start()
+
+if __name__ == "__main__":
+    time = Time()
